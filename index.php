8553aab1c58d533742855c2ea170b3b615530ba6
lukni2 <luknilukni@gmail.com>
2015-04-24 19:42:06 +0700

    tolonglah

--- index.php
+++ index.php
@@ -5,3 +5,19 @@ Saya Devit, RedMi2 saya akan datang minggu depan, ditunggu yaa teman2.
 
 
 devittt
+=======
+Saya Devit, RedMi2 saya akan datang minggu depan, ditunggu yaa teman2
+
+
+
+
+
+
+
+Ines ditunggu Devit Di Surabaya
+bingung
+
+ganteng
+bingung,
+tolonglah
+.
